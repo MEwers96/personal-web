44cbcf70eeb50d16f8e79d495edac0655cd48d2f
Mike Ewers <mikeewers@Mikes-MacBook-Pro.local>
2020-05-14 14:48:25 -0600

    Updates

---
+++ precache-manifest.ba41b1af75291141f61f64d472af7967.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "07968c9affd9ceefe1325d59660f860a",
+    "url": "/personal-web/index.html"
+  },
+  {
+    "revision": "72eb8d3a40e4ebbf97c4",
+    "url": "/personal-web/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "7ba13909d3cd56450b62",
+    "url": "/personal-web/static/js/2.41742c21.chunk.js"
+  },
+  {
+    "revision": "58aabe32aae325c8d2caf2ff6956fc48",
+    "url": "/personal-web/static/js/2.41742c21.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "72eb8d3a40e4ebbf97c4",
+    "url": "/personal-web/static/js/main.07c86ca4.chunk.js"
+  },
+  {
+    "revision": "84bf9755c30dbba7aaf3",
+    "url": "/personal-web/static/js/runtime-main.5d8cb267.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/personal-web/static/media/logo.5d5d9eef.svg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/personal-web/precache-manifest.0dcf009fa0e55e38ef41ca3e0b2baf54.js"
+  "/personal-web/precache-manifest.ba41b1af75291141f61f64d472af7967.js"
 );
 
 self.addEventListener('message', (event) => {
